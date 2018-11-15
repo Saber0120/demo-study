ef338352bc5f0bf2667da48aa647cb90b2fea110
Kaihua.Sheng <kaihua.sheng@magustek.com>
2018-11-15 14:55:46 +0800

    demo-springboot:springboot学习

---
+++ demo-boot/demo-springboot/src/main/java/com/springboot/DemoSpringbootApplication.java
@@ -0,0 +1,19 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoSpringbootApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoSpringbootApplication.class);
+	}
+}
